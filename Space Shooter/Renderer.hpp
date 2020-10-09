35d3b7fad896e01135fe0dacb67d5a25ccf9ecec
Povilas Jablonskis <povilas.jablonskis@bs2.lt>
2020-10-09 15:12:00 +0300

    Fixed null exception

--- Space Shooter/Renderer.hpp
+++ Space Shooter/Renderer.hpp
@@ -56,37 +56,36 @@ namespace Engine
 				const auto offsetLocation7 = glGetUniformLocation(program, "model");
 				auto projection = glm::ortho(0.0f, windowWidth, 0.0f, windowHeight, 0.0f, 1.0f);
 
+				glm::mat4 model(1.f);
+				model = translate(model, glm::vec3(t->getPosition(), 0.0f));
+
+				model = translate(model, glm::vec3(0.5f * t->getWidth(), 0.5f * t->getHeight(), 0.0f));
+				model = glm::rotate(model, t->getRotationAngle(), t->getRotationAxis());
+				model = translate(model, glm::vec3(-0.5f * t->getWidth(), -0.5f * t->getHeight(), 0.0f));
+
+				model = scale(model, glm::vec3(t->getWidth(), t->getHeight(), 1.0f));
+
+				glUniform4f(offsetLocation, t->getColor().x / 255.0f, t->getColor().y / 255.0f, t->getColor().z / 255.0f, t->getColor().a);
+
+				if (t->getAnimation() != nullptr)
+				{
 					auto sprites = t->getAnimation()->getAnimation();
+					auto currentSprite = sprites.at(t->getCurrentFrame());
+					glBindTexture(GL_TEXTURE_2D, t->getAnimation()->getSpriteSheetTexture());
 
-					glm::mat4 model(1.f);
-					model = translate(model, glm::vec3(t->getPosition(), 0.0f));
+					glUniform1f(offsetLocation2, 1.0f);
 
-					model = translate(model, glm::vec3(0.5f * t->getWidth(), 0.5f * t->getHeight(), 0.0f));
-					model = glm::rotate(model, t->getRotationAngle(), t->getRotationAxis());
-					model = translate(model, glm::vec3(-0.5f * t->getWidth(), -0.5f * t->getHeight(), 0.0f));
+					auto spriteSheetSize = glm::vec2(t->getAnimation()->getSpriteSheetWidth(), t->getAnimation()->getSpriteSheetHeight());
+					glUniform4f(offsetLocation3, currentSprite.x / spriteSheetSize.x, currentSprite.y / spriteSheetSize.y, currentSprite.z / spriteSheetSize.x, currentSprite.w / spriteSheetSize.y);
+				}
+				else
+				{
+					glUniform1f(offsetLocation2, 0.0f);
+				}
 
-					model = scale(model, glm::vec3(t->getWidth(), t->getHeight(), 1.0f));
-
-					glUniform4f(offsetLocation, t->getColor().x / 255.0f, t->getColor().y / 255.0f, t->getColor().z / 255.0f, t->getColor().a);
-
-					if (t->getAnimation() != nullptr)
-					{
-						auto currentSprite = sprites.at(t->getCurrentFrame());
-						glBindTexture(GL_TEXTURE_2D, t->getAnimation()->getSpriteSheetTexture());
-
-						glUniform1f(offsetLocation2, 1.0f);
-
-						auto spriteSheetSize = glm::vec2(t->getAnimation()->getSpriteSheetWidth(), t->getAnimation()->getSpriteSheetHeight());
-						glUniform4f(offsetLocation3, currentSprite.x / spriteSheetSize.x, currentSprite.y / spriteSheetSize.y, currentSprite.z / spriteSheetSize.x, currentSprite.w / spriteSheetSize.y);
-					}
-					else
-					{
-						glUniform1f(offsetLocation2, 0.0f);
-					}
-
-					glUniformMatrix4fv(offsetLocation6, 1, GL_FALSE, value_ptr(projection));
-					glUniformMatrix4fv(offsetLocation7, 1, GL_FALSE, value_ptr(model));
-					glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+				glUniformMatrix4fv(offsetLocation6, 1, GL_FALSE, value_ptr(projection));
+				glUniformMatrix4fv(offsetLocation7, 1, GL_FALSE, value_ptr(model));
+				glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 				glUseProgram(0);
 				glBindVertexArray(0);
 			}
